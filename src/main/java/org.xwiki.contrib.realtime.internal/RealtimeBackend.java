fbc938a558007f6dad77c5de7ee2bec238737753
Caleb James DeLisle <cjd@cjdns.fr>
2015-09-21 17:04:36 +0200

    Make sure when a user does not ping the server for 40 seconds, they're properly removed

--- src/main/java/org.xwiki.contrib.realtime.internal/RealtimeBackend.java
+++ src/main/java/org.xwiki.contrib.realtime.internal/RealtimeBackend.java
@@ -16,6 +16,8 @@ import org.xwiki.model.reference.DocumentReference;
 @Named("realtime")
 public class RealtimeBackend implements WebSocketHandler
 {
+    private static final long TIMEOUT_MILLISECONDS = 30000;
+
     private Map<String, Channel> channelByName = new HashMap<String, Channel>();
     private Map<WebSocket, User> userBySocket = new HashMap<WebSocket, User>();
 
@@ -59,12 +61,30 @@ public class RealtimeBackend implements WebSocketHandler
         final WebSocket sock;
         final String name;
         final Channel chan;
+        long timeOfLastMessage;
 
         User(WebSocket ws, String name, Channel chan)
         {
             this.sock = ws;
             this.name = name;
             this.chan = chan;
+        }
+    }
+
+    private void wsDisconnect(WebSocket ws)
+    {
+        User user = userBySocket.get(ws);
+        user.chan.users.remove(user.name);
+        userBySocket.remove(ws);
+        String msgStr = user.name.length() + ":" + user.name
+            + user.chan.name.length() + ":" + user.chan.name
+            + "5:[3,0]";
+        for (User u : user.chan.users.values()) {
+            u.sock.send(msgStr);
+        }
+        user.chan.messages.add(msgStr);
+        if (user.chan.users.keySet().size() == 0) {
+            channelByName.remove(user.chan.name);
         }
     }
 
@@ -89,6 +109,21 @@ public class RealtimeBackend implements WebSocketHandler
                 }
 
                 User user = userBySocket.get(ws);
+
+                long now = System.currentTimeMillis();
+                if (user != null) {
+                    user.timeOfLastMessage = now;
+                }
+
+                // It's way too much of a pain to hunt down the setTimeout() equiv
+                // in netty so I'm just going to run the check every time something comes in
+                // on the websocket to disconnect anyone who hasn't written to the WS in
+                // more than 30 seconds.
+                for (WebSocket sock : userBySocket.keySet()) {
+                    if (now - userBySocket.get(sock).timeOfLastMessage < -TIMEOUT_MILLISECONDS) {
+                        wsDisconnect(sock);
+                    }
+                }
 
                 if (user == null) {
                     // user not registered in chan
@@ -115,19 +150,7 @@ public class RealtimeBackend implements WebSocketHandler
                     //return;
                     ws.onDisconnect(new WebSocket.Callback() {
                         public void call(WebSocket ws) {
-                            User user = userBySocket.get(ws);
-                            user.chan.users.remove(user.name);
-                            userBySocket.remove(ws);
-                            String msgStr = user.name.length() + ":" + user.name
-                                + user.chan.name.length() + ":" + user.chan.name
-                                + "5:[3,0]";
-                            for (User u : user.chan.users.values()) {
-                                u.sock.send(msgStr);
-                            }
-                            user.chan.messages.add(msgStr);
-                            if (user.chan.users.keySet().size() == 0) {
-                                channelByName.remove(user.chan.name);
-                            }
+                            wsDisconnect(ws);
                         }
                     });
                 }
@@ -156,7 +179,7 @@ public class RealtimeBackend implements WebSocketHandler
         });
     }
 
-    private static class Channel 
+    private static class Channel
     {
         final Map<String, User> users = new HashMap<String, User>();
         final List<String> messages = new LinkedList<String>();
